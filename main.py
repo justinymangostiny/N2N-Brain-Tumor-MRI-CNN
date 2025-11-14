7b5dbe358926dfd4a1fb2e08200f9884417a817c
JeongWoo You <justinymangostiny@gmail.com>
2025-11-13 18:57:01 -0800

    Fixed image error

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import pandas as pd
 from PIL import Image
 import numpy as np
 
+path = kh.dataset_download("masoudnickparvar/brain-tumor-mri-dataset")
+
 # List all files
 for root, dirs, files in os.walk(path):
     for file in files:
         print(os.path.join(root, file))
 
 # Load and inspect an image
-img = Image.oppen(os.path.join)
+img = Image.open(os.path.join(path, 'some_subdir', 'image_file.jpg'))
+img_array = np.array(img)
+print(img_array.shape)
 
-path = kh.dataset_download("masoudnickparvar/brain-tumor-mri-dataset")
-print("Path to dataset files:", path)
