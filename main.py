cec4cbf3c0a71397e36ecf360d127d4288e4c0bf
JeongWoo You <justinymangostiny@gmail.com>
2025-11-13 18:49:31 -0800

    test

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import pandas as pd
 from PIL import Image
 import numpy as np
 
+# List all files
+for root, dirs, files in os.walk(path):
+    for file in files:
+        print(os.path.join(root, file))
 
+# Load and inspect an image
+img = Image.oppen(os.path.join)
 
 path = kh.dataset_download("masoudnickparvar/brain-tumor-mri-dataset")
 print("Path to dataset files:", path)
-
