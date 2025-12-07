79c5649981867395b54367ea54634dcf91e77713
JeongWoo You <justinymangostiny@gmail.com>
2025-12-06 16:45:07 -0800

    Save

--- main.py
+++ main.py
@@ -5,17 +5,7 @@ import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import tensorflow as tf
 
-train_dir = "data/Training"
-test_dir = "data/Testing"
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-
-trainset = torchvision.datasets.CIFAR10(root = train_dir, )
-testset = torchvision.datasets.CIFAR10(root = test_dir)
-
-batch_size = 4
 
